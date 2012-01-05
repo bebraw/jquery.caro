0885854b6e82626133e5a33e2c0a072959eecaf8
Juho Vepsalainen <bebraw@gmail.com>
2012-01-05 19:01:16 +0200

    Decompose code

--- js/jquery.caro.js
+++ js/jquery.caro.js
@@ -1,76 +1,97 @@
 /* MIT (c) Juho Vepsalainen */
 (function($){
-  function caroize($elem, opts) {
+  function horizontalCaro($elem, opts) {
+    caroize($elem, opts, 'left', 'width');
+  }
+
+  function verticalCaro($elem, opts) {
+    caroize($elem, opts, 'top', 'height');
+  }
+
+  function caroize($elem, opts, dir, axis) {
     var $slideContainer = $elem.find('.slides');
     var $slides = $slideContainer.children().append($('<div>'));
     var $wrapper = $('<div>').append($slides).appendTo($slideContainer);
-    var delay = opts.delay;
     var pos = 0;
     var amount = $slides.length;
     var $navi = $elem.find('.navi');
-    var dir = $elem.find('.up').length? 'top': 'left';
+
+    initCSS($slideContainer, axis, amount, $wrapper, dir, $slides);
+    initTitles($slides, $wrapper, $navi, amount, dir, opts.delay, pos);
+    initNavi($elem, $wrapper, $navi, amount, dir, opts.delay, pos);
+  }
+
+  function initCSS($slideContainer, axis, amount, $wrapper, dir, $slides) {
+    var wrapperOpts;
+    var slideOpts;
+    var displayMode;
 
     $slideContainer.css('overflow', 'hidden');
-    $wrapper.css({
-      position: 'relative',
-      height: (amount * 100) + '%', // vertical case
-      width: (amount * 100) + '%' // horizontal case
-    });
+    wrapperOpts = {
+      position: 'relative'
+    };
+    wrapperOpts[axis] = amount * 100 + '%';
+    $wrapper.css(wrapperOpts);
 
-    var displayMode = dir == 'top'? 'auto': 'inline-block';
-    $slides.each(function(i, e) {
-      $(e).css({
-        display: displayMode,
-        height: (100 / amount) + '%', // vertical case
-        width: (100 / amount) + '%', // horizontal case
-        'vertical-align': 'top'
-      });
-    });
+    displayMode = dir == 'top'? 'auto': 'inline-block';
+    slideOpts = {
+      display: displayMode,
+      'vertical-align': 'top'
+    };
+    slideOpts[axis] = (100 / amount) + '%';
+    $slides.each(function(i, e) {$(e).css(slideOpts);});  
+  }
 
-    $elem.find('.left,.up').bind('click', function() {
-      moveTo(pos - 1);
-    });
-    
-    $elem.find('.right,.down').bind('click', function() {
-      moveTo(pos + 1);
-    });
-
-    function moveTo(i) {
-      var animProps = {};
-
-      pos = Math.min(Math.max(i, 0), amount - 1);
-      
-      animProps[dir] = (pos * -100) + '%';
-      $wrapper.animate(animProps, delay);
-
-      updateNavi(pos);
-    }
-
-    function updateNavi(i) {
-      var $numberButtons = $navi.find('.number.button');
-      
-      $numberButtons.removeClass('selected');
-      $numberButtons.eq(i).addClass('selected');
-    }
-    
-    $pages = $slides.each(function(i, k) {
+  function initTitles($slides, $wrapper, $navi, amount, dir, delay, pos) {
+    $slides.each(function(i, k) {
       var title = $(k).attr('title') || i + 1;
 
       $('<div>').css('display', 'inline').text(title).bind('click', function() {
-        moveTo(i);
-      }).appendTo($navi).addClass('number button');
+        pos = moveTo(i, $wrapper, $navi, amount, dir, delay);
+      }).appendTo($navi).addClass('title button');
     });
-    updateNavi(0);
+    updateNavi(0, $navi);
   }
+
+  function initNavi($elem, $wrapper, $navi, amount, dir, delay, pos) {
+    $elem.find('.left,.up').bind('click', function() {
+      pos = moveTo(pos - 1, $wrapper, $navi, amount, dir, delay);
+    });
+
+    $elem.find('.right,.down').bind('click', function() {
+      pos = moveTo(pos + 1, $wrapper, $navi, amount, dir, delay);
+    });  
+  }
+
+  function moveTo(i, $wrapper, $navi, amount, dir, delay) {
+    var animProps = {};
+    var pos = Math.min(Math.max(i, 0), amount - 1);
+      
+    animProps[dir] = (pos * -100) + '%';
+    $wrapper.animate(animProps, delay);
+
+    updateNavi(pos, $navi);
   
+    return pos;
+  }
+
+  function updateNavi(i, $navi) {
+    var $titles = $navi.find('.title.button');
+
+    $titles.removeClass('selected');
+    $titles.eq(i).addClass('selected');
+  }
+
+
   $.fn.caro = function (options) {
     return this.each(function () {
       var $elem = $(this);
       var opts = $.extend({
         delay: 300 // in ms
       }, options);
-      
-      caroize($elem, opts);
+
+      var caro = $elem.find('.up').length? verticalCaro: horizontalCaro;
+      caro($elem, opts);
     });
   };
 })(jQuery);
